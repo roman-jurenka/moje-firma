9e6993bb6622aa05f50ec0001cf510755ad0bbe6
Roman <jurenka.elektro@gmail.com>
2026-05-09 19:51:03 +0200

    oprava klic

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://rbnqulgmywtvuryabzjc.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_KIhJk4oqQKCguH1rVgC49g_M9P0lZfg'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJibnF1bGdteXd0dnVyeWFiempjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3ODI3MTgsImV4cCI6MjA5MzM1ODcxOH0.Nl-CLAqRLQQNPSfauzBC0CyJ61Yd7JBrEuIdeK6Sudg'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
